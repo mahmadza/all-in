309d70411974b9246febbb40bca505376d071f2c
Muhammad Zabidi <mamduh.zabidi@gmail.com>
2022-11-11 19:48:54 +0800

    Recreated synthetic data

--- GenerateSyntheticData.R
+++ GenerateSyntheticData.R
@@ -1,8 +1,23 @@
 
-
 # Generate synthetic data of the IPO data sets using Synthpop in R
+setwd('Documents/GitHub/all-in/')
 library("synthpop")
+library("dplyr")
 
 real_data <- read.csv('IPO_2017to2022.csv')
-synth_data <- syn(real_data)
-write.csv(synth_data$syn, 'IPO_2017to2022_synthetic.csv')
+
+# reshape real data and generate synthetic data
+real_data <- subset(real_data, select = -c(TICKER) )
+real_data <- real_data[-1,1:11]
+# synthesize 120 IPOs
+synth_data <- syn(real_data, k = 200, smoothing = 'spline')$syn
+
+# resort
+synth_data<-synth_data[order(synth_data$YEAR, decreasing = TRUE),]  
+
+# recreate column TICKER and move to the first position
+synth_data$TICKER<-1:nrow(synth_data)
+synth_data <- synth_data %>% select(TICKER, everything())
+
+# write to file
+write.csv(synth_data, 'IPO_2017to2022_synthetic.csv', row.names=FALSE)
