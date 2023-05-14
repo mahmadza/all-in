2b14bfafa09bf412ddd3806a23e00e5eeb2ba064
Muhammad Zabidi <mamduh.zabidi@gmail.com>
2023-05-14 12:16:21 +0800

    added analysis on synthetic data

--- GenerateSyntheticData.R
+++ GenerateSyntheticData.R
@@ -4,12 +4,12 @@ setwd('Documents/GitHub/all-in/')
 library("synthpop")
 library("dplyr")
 
-real_data <- read.csv('IPO_2017to2022.csv')
+real_data <- read.csv('IPO_2017to2023.csv')
 
 # reshape real data and generate synthetic data
 real_data <- subset(real_data, select = -c(TICKER) )
 real_data <- real_data[-1,1:11]
-# synthesize 120 IPOs
+# synthesize 200 IPOs
 synth_data <- syn(real_data, k = 200, smoothing = 'spline')$syn
 
 # resort
@@ -20,4 +20,4 @@ synth_data$TICKER<-1:nrow(synth_data)
 synth_data <- synth_data %>% select(TICKER, everything())
 
 # write to file
-write.csv(synth_data, 'IPO_2017to2022_synthetic.csv', row.names=FALSE)
+write.csv(synth_data, 'IPO_2017to2023_synthetic.csv', row.names=FALSE)
